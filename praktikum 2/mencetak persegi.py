12d6d9d6574d06be870b5bc45e421765e873b858
mhmdrisqi <114374474+mhmdrisqi@users.noreply.github.com>
2022-12-23 18:13:42 +0700

    Add files via upload

---
+++ praktikum 2/mencetak persegi.py
@@ -0,0 +1,8 @@
+besar_persegi = int (input("Masukan sebuah nilai bilangan bulat = "))
+
+for i in range(besar_persegi):
+     for j in range(besar_persegi):
+        print("#"  "   ", end="")
+     print()
+        
+
---
+++ praktikum 2/menghitung rata-rata.py
@@ -0,0 +1,8 @@
+inp =int(input("input nilai bilangan = "))
+total = 0
+for i in range(inp):
+    nilai = int(input("masukan nilai = e"))
+    total += nilai
+
+    ratarata = total/inp
+    print("rata rata: ",ratarata)
